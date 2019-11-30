4a178aff1d152b814f7c1ffe777664769eb13698
Reyk Floeter <contact@reykfloeter.com>
2019-11-30 22:38:46 +0000

    Use ninja to build it faster

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -5,14 +5,21 @@ set -x
 
 [ -z "${LIBRESSL_DIR}" ] && exit 0
 
+# Install ninja for faster builds
+apt-get install apt-get install ninja-build
+
 mkdir -p "${LIBRESSL_DIR}"
 cd "${LIBRESSL_DIR}"
 
 curl -sSf -O "https://cdn.openbsd.org/pub/OpenBSD/LibreSSL/libressl-${LIBRESSL}.tar.gz"
 tar zxf "libressl-${LIBRESSL}.tar.gz"
 cd "libressl-${LIBRESSL}"
-./configure --prefix="${LIBRESSL_DIR}" --with-openssldir="${LIBRESSL_DIR}"
-make -j"$(nproc)"
-make install
+
+mkdir build-ninja
+cd build-ninja
+cmake -G"Ninja" --prefix "${LIBRESSL_DIR}" -D LIBRESSL_ROOT_DIR="${LIBRESSL_DIR}" ..
+ninja
+ninja test
+ninja install
 
 exit 0
