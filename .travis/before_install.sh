0e60cf31a822ce929ec2f2107b150f1ec5af67b1
Reyk Floeter <contact@reykfloeter.com>
2019-12-20 16:30:53 +0000

    Don't build shared libraries, speed up travis

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -11,7 +11,7 @@ cd "${LIBRESSL_DIR}"
 curl -sSf -O "https://cdn.openbsd.org/pub/OpenBSD/LibreSSL/libressl-${LIBRESSL}.tar.gz"
 tar zxf "libressl-${LIBRESSL}.tar.gz"
 cd "libressl-${LIBRESSL}"
-./configure --prefix="${LIBRESSL_DIR}" --with-openssldir="${LIBRESSL_DIR}"
+./configure --prefix="${LIBRESSL_DIR}" --with-openssldir="${LIBRESSL_DIR} --disable-shared"
 make -j"$(nproc)"
 make install
 
